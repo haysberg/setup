442d35981881bf99bc1a93e2b19e971319285dec
Téo Haÿs <me@haysberg.io>
2023-03-27 12:01:31 +0200

    Update fedora.sh

--- fedora.sh
+++ fedora.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
 
+
+
 echo "Ajout de VS Code..."
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
@@ -9,8 +11,14 @@ sudo dnf check-update
 sudo dnf remove -y *libreoffice* gnome-weather gnome-maps gnome-boxes *cheese* simple-scan rhythmbox
 sudo dnf autoremove -y
 sudo dnf update -y
-sudo dnf install -y git code dnf-plugins-core gnome-tweaks
-sudo dnf group install "Security Lab"
+sudo dnf install -y git code dnf-plugins-core gnome-tweaks dnf-plugins-core
+sudo dnf config-manager \
+    --add-repo \
+    https://download.docker.com/linux/fedora/docker-ce.repo
+
+sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo usermod -aG docker $USER
 
 flatpak update -y
 
@@ -31,3 +39,6 @@ io.github.shiftey.Desktop \
 com.brave.Browser
 
 hostnamectl set-hostname thiccpad
+
+# Install Docker
+

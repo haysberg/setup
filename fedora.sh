837d68534f7f9b56846d1245189dca678685cac6
Téo Haÿs <me@haysberg.io>
2023-04-02 01:09:55 +0200

    Added Jetbrains Mono

--- fedora.sh
+++ fedora.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-echo "Ajout de VS Code..."
+
+echo "Setup Jetbrains Mono..."
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/JetBrains/JetBrainsMono/master/install_manual.sh)"
+
+gsettings set org.gnome.desktop.interface monospace-font-name 'JetBrains Mono 11'
+
+echo "Repo VSCode..."
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 
+echo "Installation des paquets..."
 sudo dnf check-update
 sudo dnf remove -y *libreoffice* gnome-weather gnome-maps gnome-boxes *cheese* simple-scan rhythmbox
 sudo dnf autoremove -y

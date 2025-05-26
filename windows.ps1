ccc15e20c9971ae966e0a482f077d6910e893648
Téo Haÿs <teo@haysberg.io>
2025-05-26 20:13:12 +0000

    Update windows.ps1

--- windows.ps1
+++ windows.ps1
@@ -1,29 +1,27 @@
-if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+# List of package IDs to install
+$packageIds = @(
+    "Discord.Discord",
+    "Spotify.Spotify",
+    "Valve.Steam",
+    "flux.flux",
+    "Project.EarTrumpet",
+    "OpenWhisperSystems.Signal",
+    "LibreWolf.LibreWolf",
+    "Jellyfin.JellyfinMediaPlayer"
+)
 
-Write-Output("Installation de Chocolatey...")
-# Run your code that needs to be elevated here...
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-
-choco feature enable -n=allowGlobalConfirmation
-choco feature enable -n=useRememberedArgumentsForUpgrades
-
-Write-Output("Installation des programmes...")
-$chocojob = Start-Job {
-    choco install discord spotify steam f.lux.install eartrumpet
+# Loop through each package ID and start a job for each installation
+foreach ($packageId in $packageIds) {
+    Start-Job -ScriptBlock {
+        param($id)
+        winget install --id $id --accept-package-agreements --accept-source-agreements
+    } -ArgumentList $packageId
 }
 
 Write-Output("Telechargement & Installation de LoL...")
-$job1 = Start-Job {
+$loljob = Start-Job {
     Invoke-WebRequest -Uri "https://lol.secure.dyn.riotcdn.net/channels/public/x/installer/current/live.euw.exe" -OutFile "$HOME\Downloads\lol_install.exe"
     Start-Process -Filepath "$HOME\Downloads\lol_install.exe"
 }
 
-Write-Output("Telechargement & Installation de NGENUITY...")
-$job2 = Start-Job {
-    Invoke-WebRequest -Uri "https://hyperx.gg/ngenuity-installer" -OutFile "$HOME\Downloads\ngenuity.exe"
-    Start-Process -Filepath "$HOME\Downloads\ngenuity.exe"
-}
-
-Wait-Job $job1
-Wait-Job $job2
-Wait-Job $chocojob
+Get-Job | Wait-Job | Receive-Job

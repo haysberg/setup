68c57b010076bb000f93d28ee571aa683031fcce
Téo Haÿs <teo@haysberg.io>
2025-05-28 14:42:23 +0000

    Update windows.ps1
    Simplifying script

--- windows.ps1
+++ windows.ps1
@@ -1,29 +1,11 @@
 Start-Process "https://www.amd.com/en/support/download/drivers.html"
 
-# List of package IDs to install
-$packageIds = @(
-    "Discord.Discord",
-    "Spotify.Spotify",
-    "Valve.Steam",
-    "flux.flux",
-    "Project.EarTrumpet",
-    "OpenWhisperSystems.Signal",
-    "LibreWolf.LibreWolf",
-    "Jellyfin.JellyfinMediaPlayer"
-)
-
-# Loop through each package ID and start a job for each installation
-foreach ($packageId in $packageIds) {
-    Start-Job -ScriptBlock {
-        param($id)
-        winget install --id $id --accept-package-agreements --accept-source-agreements
-    } -ArgumentList $packageId
-}
-
-Write-Output("Telechargement & Installation de LoL...")
-$loljob = Start-Job {
-    Invoke-WebRequest -Uri "https://lol.secure.dyn.riotcdn.net/channels/public/x/installer/current/live.euw.exe" -OutFile "$HOME\Downloads\lol_install.exe"
-    Start-Process -Filepath "$HOME\Downloads\lol_install.exe"
-}
-
-Get-Job | Wait-Job | Receive-Job
+winget install -e --id RiotGames.LeagueOfLegends.EUW
+winget install -e --id File-New-Project.EarTrumpet
+winget install -e --id Discord.Discord
+winget install -e --id Spotify.Spotify
+winget install -e --id Valve.Steam
+winget install -e --id flux.flux
+winget install -e --id OpenWhisperSystems.Signal
+winget install -e --id LibreWolf.LibreWolf
+winget install -e --id Jellyfin.JellyfinMediaPlayer

5c23e4e72d09265653dbae483a512bc307962a5b
Téo Haÿs <teo@haysberg.io>
2025-05-28 14:45:19 +0000

    Added QoL changes

--- windows.ps1
+++ windows.ps1
@@ -9,3 +9,16 @@ winget install -e --id flux.flux
 winget install -e --id OpenWhisperSystems.Signal
 winget install -e --id LibreWolf.LibreWolf
 winget install -e --id Jellyfin.JellyfinMediaPlayer
+
+$shell = New-Object -ComObject "Shell.Application"
+$terminalPath = "ms-windows-store://pdp/?ProductId=9N0DX20HK701"
+$terminal = $shell.NameSpace($terminalPath).Self
+$terminal.Invokeverb("taskbarpin")
+
+$shell = New-Object -ComObject "Shell.Application"
+$taskmgrPath = "C:\Windows\System32\Taskmgr.exe"
+$taskmgr = $shell.NameSpace($taskmgrPath).Self
+$taskmgr.Invokeverb("taskbarpin")
+
+# Enable Do Not Disturb mode
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Notifications\Settings" -Name "NOC_GLOBAL_SETTING_TOASTS_ENABLED" -Value 0

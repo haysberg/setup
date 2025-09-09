9e02ebfc036924272390517431d51b74a3509968
Téo Haÿs <teo@haysberg.io>
2025-09-09 19:55:53 +0200

    Update windows.ps1

--- windows.ps1
+++ windows.ps1
@@ -11,11 +11,7 @@ winget install -e --id Valve.Steam
 winget install -e --id flux.flux
 winget install -e --id OpenWhisperSystems.Signal
 winget install -e --id Jellyfin.JellyfinMediaPlayer
-winget install -e --id VideoLAN.VLC
 winget install -e --id ProtonTechnologies.ProtonVPN
-winget install -e --id Microsoft.VisualStudioCode
-winget install -e --id GitHub.GitHubDesktop
-winget install -e --id kalilinux.kalilinux
 
 # Enable Do Not Disturb mode
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Notifications\Settings" -Name "NOC_GLOBAL_SETTING_TOASTS_ENABLED" -Value 0

08d5df34071f59fdae95dc2f8c90111108146727
Téo <teo@device-2008.home>
2025-09-09 21:39:24 +0200

    Refacto

--- fedora.sh
+++ fedora.sh
@@ -16,15 +16,15 @@ sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
 
 echo "Installation des paquets..."
+sudo dnf copr enable lilay/topgrade -y
 sudo dnf check-update
 sudo dnf remove -y *libreoffice* gnome-weather gnome-photos gnome-calendar gnome-maps gnome-boxes *cheese* simple-scan rhythmbox
 sudo dnf autoremove -y
+sudo dnf install -y git code topgrade dnf-plugins-core distrobox
 sudo dnf update -y
 
-sudo dnf copr enable lilay/topgrade
-sudo dnf install -y git code topgrade dnf-plugins-core dnf-plugins-core clang distrobox
-
 curl -LsSf https://astral.sh/uv/install.sh | sh
-topgrade -cy
 
 distrobox create --name kali --image kalilinux/kali-rolling
+
+topgrade -cy

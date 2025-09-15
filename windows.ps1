623627af2e90bebf361ec694bb4e72dcae28fb8b
Téo Haÿs <teo@haysberg.io>
2025-09-15 15:16:40 +0200

    Update windows.ps1

--- windows.ps1
+++ windows.ps1
@@ -1,17 +1,18 @@
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+Write-Output("Installation de Chocolatey...")
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+
+
+choco feature enable -n=allowGlobalConfirmation
+
+choco feature enable -n=useRememberedArgumentsForUpgrades
+
 # To be used after activation / AtlasOS run
 
 Start-Process "https://www.amd.com/en/support/download/drivers.html"
 
-winget install -e --id RiotGames.LeagueOfLegends.EUW
-winget install -e --id RiotGames.Valorant.EU
-winget install -e --id File-New-Project.EarTrumpet
-winget install -e --id Discord.Discord
-winget install -e --id Spotify.Spotify
-winget install -e --id Valve.Steam
-winget install -e --id flux.flux
-winget install -e --id OpenWhisperSystems.Signal
-winget install -e --id Jellyfin.JellyfinMediaPlayer
-winget install -e --id ProtonTechnologies.ProtonVPN
+choco install discord spotify steam f.lux.install eartrumpet amd-ryzen-chipset signal jellyfin-media-player
 
 # Enable Do Not Disturb mode
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Notifications\Settings" -Name "NOC_GLOBAL_SETTING_TOASTS_ENABLED" -Value 0

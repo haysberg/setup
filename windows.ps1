e9919d5df6301d67ffd824fe53a942353ab87c13
Téo Haÿs <teo@haysberg.io>
2025-10-12 09:40:53 +0200

    Update windows.ps1

--- windows.ps1
+++ windows.ps1
@@ -2,17 +2,23 @@ if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
 Write-Output("Installation de Chocolatey...")
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-
+Write-Output("Telechargement & Installation de 2XKO...")
+$job1 = Start-Job {
+    Invoke-WebRequest -Uri "https://lion.secure.dyn.riotcdn.net/channels/public/x/installer/current/live.live.eu.exe" -OutFile "$HOME\Downloads\2xko.exe"
+    Start-Process -Filepath "$HOME\Downloads\2xko.exe"
+}
 
 choco feature enable -n=allowGlobalConfirmation
-
 choco feature enable -n=useRememberedArgumentsForUpgrades
 
 # To be used after activation / AtlasOS run
 
 Start-Process "https://www.amd.com/en/support/download/drivers.html"
+Start-Process "https://hyperx.com/pages/ngenuity"
 
-choco install discord spotify steam f.lux.install eartrumpet amd-ryzen-chipset signal jellyfin-media-player
+choco install discord spotify steam f.lux.install eartrumpet signal jellyfin-media-player
 
 # Enable Do Not Disturb mode
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Notifications\Settings" -Name "NOC_GLOBAL_SETTING_TOASTS_ENABLED" -Value 0
+
+Wait-Job $job1

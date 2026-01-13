f8d998229f3acd1a8a857159d9681eb0e0c3d5be
Téo Haÿs <29246792+haysberg@users.noreply.github.com>
2026-01-13 18:46:23 +0100

    Enhance windows.ps1 for software installations
    Updated script to include additional software installations using winget and modified download process for 2XKO and Affinity.

--- windows.ps1
+++ windows.ps1
@@ -1,15 +1,35 @@
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+# To be used after AtlasOS
+Start-Process "https://www.amd.com/en/support/download/drivers.html"
 
 Write-Output("Telechargement & Installation de 2XKO...")
 $job1 = Start-Job {
     Invoke-WebRequest -Uri "https://lion.secure.dyn.riotcdn.net/channels/public/x/installer/current/live.live.eu.exe" -OutFile "$HOME\Downloads\2xko.exe"
     Start-Process -Filepath "$HOME\Downloads\2xko.exe"
-    Invoke-WebRequest -Uri "https://downloads.affinity.studio/Affinity%20x64.exe" -OutFile "$HOME\Downloads\affinity.exe"
-    Start-Process -Filepath "$HOME\Downloads\affinity.exe"
 }
 
-# To be used after AtlasOS
-Start-Process "https://www.amd.com/en/support/download/drivers.html"
+$winget_packages = @(
+    "xmn.BetterTrumpet ",
+    "TIDALMusicAS.TIDAL",
+    "Valve.Steam",
+    "flux.flux",
+    "OpenWhisperSystems.Signal",
+    "Jellyfin.JellyfinMediaPlayer",
+    "Telegram.TelegramDesktop",
+    "Proton.ProtonDrive",
+    "Vencord.Vesktop",
+    "Canva.Affinity",
+    "Microsoft.WindowsTerminal"
+)
 
-choco install -y f.lux tidal steam eartrumpet signal telegram protondrive dorion jellyfin-media-player
+$jobs = @()
+foreach ($package in $winget_packages) {
+    $jobs += Start-Job -ScriptBlock {
+        param($pkg)
+        winget install -e --id $pkg --accept-source-agreements --accept-package-agreements
+    } -ArgumentList $package
+}
+
+Write-Output "Waiting for winget installations to complete..."
+Wait-Job -Job $jobs
+Write-Output "All winget installations are complete."
 Wait-Job $job1

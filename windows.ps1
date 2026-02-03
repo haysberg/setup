a16510fee1a41d45fade1e75c5dfc0ba9585d182
Téo Haÿs <29246792+haysberg@users.noreply.github.com>
2026-02-03 18:06:17 +0100

    Clean up Xbox App and Game Bar configurations
    Removed Xbox App removal commands and disabled Game Bar settings.

--- windows.ps1
+++ windows.ps1
@@ -20,9 +20,9 @@ $winget_packages = @(
     "Proton.ProtonDrive",
     "Vencord.Vesktop",
     "Canva.Affinity",
-    "Microsoft.WindowsTerminal",
     "topgrade-rs.topgrade",
-    "ImputNet.Helium"
+    "ImputNet.Helium",
+    "9NZKPSTSNW4P --source msstore"
 )
 
 Start-Process powershell -Verb RunAs -ArgumentList "-NoExit", "-Command", "Install-Module PSWindowsUpdate"
@@ -34,23 +34,6 @@ foreach ($package in $winget_packages) {
         winget install -e --id $pkg --accept-source-agreements --accept-package-agreements
     } -ArgumentList $package
 }
-
-# Remove Xbox App
-Get-AppxPackage Microsoft.XboxGamingOverlay | Remove-AppxPackage
-
-# Disable ms-gamingoverlay links
-reg add HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\GameDVR /f /t REG_DWORD /v "AppCaptureEnabled" /d 0
-reg add HKEY_CURRENT_USER\System\GameConfigStore /f /t REG_DWORD /v "GameDVR_Enabled" /d 0
-
-# Disable ms-gamebar links
-reg add HKCR\ms-gamebar /f /ve /d URL:ms-gamebar 2>&1 >''
-reg add HKCR\ms-gamebar /f /v "URL Protocol" /d "" 2>&1 >''
-reg add HKCR\ms-gamebar /f /v "NoOpenWith" /d "" 2>&1 >''
-reg add HKCR\ms-gamebar\shell\open\command /f /ve /d "\`"$env:SystemRoot\System32\systray.exe\`"" 2>&1 >''
-reg add HKCR\ms-gamebarservices /f /ve /d URL:ms-gamebarservices 2>&1 >''
-reg add HKCR\ms-gamebarservices /f /v "URL Protocol" /d "" 2>&1 >''
-reg add HKCR\ms-gamebarservices /f /v "NoOpenWith" /d "" 2>&1 >''
-reg add HKCR\ms-gamebarservices\shell\open\command /f /ve /d "\`"$env:SystemRoot\System32\systray.exe\`"" 2>&1 >''
 
 
 Write-Output "Waiting for winget installations to complete..."

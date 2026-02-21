235fb7da16a1029e38fe02d95b104653af2bca10
Téo Haÿs <29246792+haysberg@users.noreply.github.com>
2026-02-21 21:36:20 +0100

    Refactor fedora.sh for macOS compatibility
    Updated package installation commands for macOS using Homebrew and modified Flatpak applications.

--- fedora.sh
+++ fedora.sh
@@ -2,25 +2,17 @@
 
 sudo hostnamectl set-hostname --static linucc
 
-echo "Installation des paquets..."
-sudo dnf copr enable lilay/topgrade -y
-sudo dnf check-update
-sudo dnf remove -y *libreoffice* gnome-weather gnome-photos gnome-calendar gnome-maps gnome-boxes *cheese* simple-scan rhythmbox
-sudo dnf autoremove -y
-sudo dnf install -y git topgrade
-
-flatpak remote-delete fedora
-flatpak remote-delete fedora-testing
-flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+brew install topgrade
 
 topgrade -cy
 
 flatpak install flathub -y \
-com.mastermindzh.tidal-hifi \
+com.spotify.Client \
 org.telegram.desktop \
 org.signal.Signal \
-com.github.iwalton3.jellyfin-media-player \
+org.jellyfin.JellyfinDesktop \
 dev.vencord.Vesktop \
 dev.zed.Zed
 
 curl -LsSf https://astral.sh/uv/install.sh | sh
+
